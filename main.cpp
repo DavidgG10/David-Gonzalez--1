8ba659bb41019b5601efef10d92e8bcc0b5c6949
DavidG <72959512+DavidgG10@users.noreply.github.com>
2021-11-23 19:02:29 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int main() { 
+using namespace std;
+int casa=0; 
+int carro=0;
+//se crean las variables 
+cout << "digite un numero "; 
+
+cin >>casa;  
+cout << "digite un numero "; 
+cin >>carro; 
+ if (casa>carro) { 
+cout << casa << " es mayor " << carro; 
+}  
+else { 
+cout <<casa <<" es menor " << carro; 
+
+
+}  
+
+
+
+
+} 
